954d2e626f4d58959cf371c111fbcf7eeac6cf39
Anisha Bheemisetty <anishabheemisetty.1234@gmail.com>
2023-03-19 15:50:33 +0530

    Week 8 Assignment

---
+++ Week-8/greatestElement.js
@@ -0,0 +1,35 @@
+
+function nextLargerElement(arr, n)
+{
+	var s = [];
+	
+	// iterating over the array
+	for (var i = 0; i < arr.length; i++)
+	{
+		while (s.length > 0 && s[s.length - 1]["value"] < arr[i])
+		{
+			// updating the array as per the stack top
+			var d = s.pop();
+			arr[d["ind"]] = arr[i];
+		}
+		// pushing values to stack
+		s.push({"value": arr[i], "ind": i});
+	}
+	
+	// updating the array as per the stack top
+	while (s.length > 0)
+	{
+		d = s.pop();
+		arr[d["ind"]] = -1;
+	}
+	return arr;
+	
+}
+
+// Driver Code
+var arr = [1,3,2,4];
+var n = 5;
+
+// Function call
+console.log(nextLargerElement(arr, n));
+
